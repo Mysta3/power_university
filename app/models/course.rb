ebac3be4147eb69018f4b8080f06949816f2e505
mysta3 <myshawnestallings@gmail.com>
2021-05-06 15:42:05 -0700

    added partials

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,11 @@
 class Course < ApplicationRecord 
-  
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :short_name, presence: true, length: { minimum: 5, maximum: 15 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 250 }
+
+  def index
+    @courses = Course.all
+  end
 end
+
+  
